48305655418c9d38897bba8e553fed16cbc4c2b9
eriawan <eriawan@gti.co.id>
2022-01-22 17:08:58 +0700

    adding donation payment email

---
+++ resources/views/emails/donor.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+
+# Halo, {{$donorName}}
+
+Terimakasih sudah mau berpartisipasi dalam memelihara hutan Indonesia dengan berdonasi di Lindungi Hutan.<br>
+Untuk melakukan pembayaran donasi, anda dapat transfer melalui rekening berikut:<br><br>
+    No Rekening &emsp;: 1029384756<br>
+    Bank &emsp;&emsp;&emsp;&emsp;: BCA<br>
+    Jumlah &emsp;&emsp;&emsp;&emsp;: {{$donationAmount}}
+
+Harap melakukan pembayaran dalam waktu 1x24 jam.
+
+Thanks,<br>
+Lindungi Hutan
+@endcomponent
